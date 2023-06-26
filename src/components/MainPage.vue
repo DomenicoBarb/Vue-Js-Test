4ac2a1c4aaee686c155695e7e05a6cdce0699cbb
DomenicoBarb <domenicobarberi585@gmail.com>
2023-06-26 03:19:49 -0400

    99% similar to React version, minor text tweaks required

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,12 +4,14 @@
             <AntRow align="top" :gutter="16">
                 <AntCol :lg="isMobile ? 24 : isTablet ? 24 : 6" :xs="24" :md="24">
                     <div class="portrait-container">
-                        <div class="portrait-wrapper">
+                        <div class="portrait-wrapper" :style="{
+                       paddingBottom: isMobile || isTablet ? '20px' : '0px'
+                    }" >
                             <img :src="portraitImage" alt="Portrait" title="Not a picture of me but I do have a moustache"
                                 class="portrait-image" :style="{
                                     marginTop: isMobile || isTablet ? '120px' : '140px',
-                                    width: isMobile || isTablet ? '40%' : '90%',
-                                    marginLeft: isMobile || isTablet ? '0px' : '30px'
+                                    width: isMobile || isTablet ? '72%' : '90%',
+                                    marginLeft: isMobile || isTablet ? '0px' : '34px'
                                 }" />
                         </div>
                     </div>
@@ -17,7 +19,7 @@
                 <AntCol :xs="24" :lg="isMobile ? 24 : isTablet ? 24 : 18">
                     <h1 class="page-header" :style="{
                         color: '#FFF',
-                        paddingTop: isMobile || isTablet ? '10px' : '96px',
+                        paddingTop: isMobile || isTablet ? '10px' : '82px',
                         fontWeight: '450',
                         textAlign: 'center',
                         marginLeft: isMobile || isTablet ? '0px' : '0px'
@@ -33,10 +35,13 @@
                             score. The fewer turns it takes, the better! You can checkout it out on my GitHub page linked in the footer.
                         </p>
                         <p class="bio-text-formatting">
-                            Here is some placeholder text to see how the page looks with more text. I will use the Ant Design Typography component below this!
+                            Here is some placeholder text to see how the page looks with more text, it is using a  &lt;p&gt;&lt;/p&gt; tag. I will use the Ant Design Typography component below this!
                         </p>
                         <Typography class="bio-text-formatting">
-                                This is using the Ant Design Typography component. It seems like Ant Design for Vue is a little different to the React version. For example I used the Paragraph component instead of the Typography component.
+                                This is using the Ant Design Typography component. I have to use it as it seems like Ant Design for Vue is a little different from the React version. For example I used the Paragraph component instead of the Typography component in the React equivalent to this.
+                        </Typography>
+                        <Typography class="bio-text-formatting">
+                                The Ant Design Typography component seems to have some quirks as well, it is registered as an &lt;article&gt;&lt;/article&gt; so I need to designate article styling for it to look the same as the standard &lt;p&gt;&lt;/p&gt; tags used above.
                         </Typography>
                         <!-- Rest of the bio-text -->
                     </div>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line
 const { defineConfig } = require('@vue/cli-service')
+// eslint-disable-next-line
 module.exports = defineConfig({
   transpileDependencies: true
 })
