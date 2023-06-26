ed0fd31ce7d6fe06e0c1a0127cc4ce618f4fd973
DomenicoBarb <domenicobarberi585@gmail.com>
2023-06-26 10:57:43 -0400

    tweaked some text sizes, fighting some sort of scaling

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,14 +1,25 @@
 <template>
-    <header class="navbar">
-      <div class="header">
+  <header class="navbar">
+    <div class="header">
         <h2 class="header-title" title="You found an Easter egg! Are there other ones? 🤔">
           Domenico Barberi
         </h2>
-      </div>
-    </header>
-  </template>
+    </div>
+  </header>
+</template>
   
-  <style scoped>
-  @import "../styles/global.css";
-  </style>
+
+<script>
+// import { Typography } from 'ant-design-vue';
+
+// export default {
+//   components: {
+//     'a-typography': Typography,
+//   },
+// }
+</script>
+
+<style>
+@import "../styles/global.css";
+</style>
   
