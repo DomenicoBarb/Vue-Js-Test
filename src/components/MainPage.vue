6f59b0a217892caa8810ae1acfdb57d3e518be80
DomenicoBarb <domenicobarberi585@gmail.com>
2023-07-13 15:46:53 -0400

    adjusted footer size, added portfolio/react version link and added hover animation to footer icons

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -28,22 +28,26 @@
                     </h1>
                     <div class="bio-formatting">
                         <p class="bio-text-formatting">
-                            This is a test page to experiment with Ant Design and Vue. I am a Full-Stack Web Developer with
+                            This is a test page to experiment with Vue.js and Ant Design to make a simple responsive page. I am a Full-Stack Web Developer (MERN) with
                             a passion for learning new technologies and solving problems. I have another project that used
-                            Ant Design called Recall Rumble! It was the final project in my coding bootcamp that I was lucky
+                            Ant Design (within React) called Recall Rumble! It was the final project in my coding bootcamp that I was lucky
                             enough to lead! It is a cool concentration-like card game where the player matches cards for a
-                            score. The fewer turns it takes, the better! You can checkout it out on my GitHub page linked in the footer.
+                            score. The fewer turns it takes, the better! You can checkout it out on my portfolio linked in the footer.
                         </p>
                         <p class="bio-text-formatting">
-                            Here is some placeholder text to see how the page looks with more text, it is using a  &lt;p&gt;&lt;/p&gt; tag. I will use the Ant Design Typography component below this!
+                            Here is some placeholder text to experiment with Ant Design within Vue as it has some differences from the React version, here I am using a  &lt;p&gt;&lt;/p&gt; tag. Below this I will use the Ant Design Typography component.
                         </p>
                         <Typography class="bio-text-formatting">
-                                This is using the Ant Design Typography component. I have to use it as it seems like Ant Design for Vue is a little different from the React version. For example I used the Paragraph component instead of the Typography component in the React equivalent to this.
+                                This block of text is using the Ant Design &lt;Typography&gt; component.
+                                I am using it in place of the Ant Design Paragaph tag as it seems like Ant Design for Vue is a little different from the React version.
+                                The Paragraph tag doesn't seem to exist within the Vue version.
                         </Typography>
                         <Typography class="bio-text-formatting">
-                                The Ant Design Typography component seems to have some quirks as well, it is registered as an &lt;article&gt;&lt;/article&gt; so I need to designate article styling for it to look the same as the standard &lt;p&gt;&lt;/p&gt; tags used above.
+                                The Ant Design Typography component seems to have some quirks as well, it is registered as an &lt;article&gt;&lt;/article&gt; so I need to designate article styling for it to look the same as the standard &lt;p&gt;&lt;/p&gt; tags used above. That seems a little counter-intuitive to me but I am sure there is a reason for it, just complicates styling a little bit.
                         </Typography>
-                        <!-- Rest of the bio-text -->
+                        <Typography class="bio-text-formatting">
+                                I have also added the React version I made of this simple about me page to the footer below. I wanted to explore making the same app within React and Vue for fun! It was also to experiment with deploying the AWS (Amplify).
+                        </Typography>
                     </div>
                 </AntCol>
             </AntRow>
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -8,6 +8,12 @@
                 <a href="https://www.linkedin.com/in/domenico-a-barberi/" target="_blank" rel="noopener noreferrer">
                     <img src="../assets/white-linked-in.png" alt="LinkedIn" class="logo" title="My LinkedIn profile" />
                 </a>
+                <a href="https://domenico-barberi-react-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <img src="../assets/logo512.webp" alt="ReactPortfolio" class="portfoliologo" title="React Portfolio" />
+                </a>
+                <a href="https://dev.d3cbjkk0ofnz30.amplifyapp.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="../assets/react.png" alt="ReactVersion" class="logo" title="About Me React Version" />
+                </a>
             </Space>
         </div>
     </footer>
